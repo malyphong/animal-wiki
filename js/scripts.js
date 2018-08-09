460bdcb346235d0ac2a92828a6dd3acb62434de7
Evan Filkins and Maly Phongsavanh <maly@>
2018-08-09 09:52:01 -0700

    Create HTML doc with CSS and JS scripts to toggle displayed elements.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('input#sloth').click(function() {
+    $('#sloth-info').fadeToggle();
+  });
+  $('input#giraffe').click(function() {
+    $('#giraffe-info').fadeToggle();
+  });
+  $('input#unicorn').click(function() {
+    $('#unicorn-info').fadeToggle();
+  });
+});
